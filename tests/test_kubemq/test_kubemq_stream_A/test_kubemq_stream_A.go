1ab37f603f98ecc504d31aafa2afcb89ef7471b5
Richard Jayne <rjayne@msn.com>
2022-05-09 09:18:14 -0700

    Added single client, single channel test as test_kubemq_stream_A.go.
    This test does not suffer from Ack EOF error.

--- tests/test_kubemq/test_kubemq_stream_A/test_kubemq_stream_A.go
+++ tests/test_kubemq/test_kubemq_stream_A/test_kubemq_stream_A.go
@@ -16,9 +16,8 @@ import (
 
 /*
 * This code uses a modified version of (https://github.com/kubemq-io/go-sdk-cookbook/blob/main/queues/stream/main.go).
-* In this test scenario there are 2 message queues (channelA, channelB) and 2 queue stream clients (queuesClientA, queuesClientB).
-* queuesClientA sends messages to channelB. Those messageIDs are added to a string set.
-* queuesClientB receives messages from channelB and sends/echos them to channelA (the hard way, not via resend).
+* In this test scenario there is 1 message queue (channelA) and 1 queue stream client (queuesClientA).
+* queuesClientA sends messages to channelA. Those messageIDs are added to a string set.
 * queuesClientA receives messages from channelA.  Those messageIDs are removed from the string set.
 * A successful test will have no errors and no 'lost' messages in the string set.
  */
@@ -83,25 +82,10 @@ func main() {
 			log.Fatalf("ERROR-A: QueuesStreamClient.Close failed, err: %v", err)
 		}
 	}()
-	// queuesClientB will recv messages from channelB and echo/send messages back to channelA.
-	queuesClientB, err := kubemq.NewQueuesStreamClient(ctx,
-		kubemq.WithAddress("localhost", 50000),
-		kubemq.WithClientId("test-kubemq-stream-B"),
-		kubemq.WithTransportType(kubemq.TransportTypeGRPC))
-	if err != nil {
-		log.Fatalf("ERROR-B: NewQueuesStreamClient failed, err: %v", err)
-	}
-	defer func() {
-		err := queuesClientB.Close()
-		if err != nil {
-			log.Fatalf("ERROR-B: QueuesStreamClient.Close failed, err: %v", err)
-		}
-	}()
 	// messageSet will track queuesClientA send/recv messages.
 	messageSet := stringSet{}
 	messageSet.init()
 	channelA := "test.kubemq.stream.A"
-	channelB := "test.kubemq.stream.B"
 	// Start queuesClientA receive and print routine.
 	doneA, err := queuesClientA.TransactionStream(ctx, &kubemq.QueueTransactionMessageRequest{
 		ClientID:          "test-kubemq-stream-receiver-A",
@@ -124,37 +108,6 @@ func main() {
 		log.Fatalf("ERROR-A: TransactionStream failed, err: %v", err)
 	}
 	defer func() { doneA <- struct{}{} }()
-	// Start queuesClientB receive and send/echo routine.
-	doneB, err := queuesClientB.TransactionStream(ctx, &kubemq.QueueTransactionMessageRequest{
-		ClientID:          "test-kubemq-stream-receiver-B",
-		Channel:           channelB,
-		VisibilitySeconds: 10,
-		WaitTimeSeconds:   10,
-	}, func(response *kubemq.QueueTransactionMessageResponse, err error) {
-		if err != nil {
-			log.Printf("ERROR-B: QueueTransactionMessageResponse failed, err: %v", err)
-		} else {
-			message := string(response.Message.Body)
-			log.Printf("RECV-B: MessageID: %s, Body: %s, Ack", response.Message.MessageID, message)
-			err = response.Ack()
-			if err != nil {
-				log.Fatalf("ERROR-B: QueueTransactionMessageResponse.Ack failed, err: %v", err)
-			}
-			// Send/echo message to channelA.
-			sendResult, err := queuesClientB.Send(ctx, kubemq.NewQueueMessage().
-				SetId(response.Message.MessageID).
-				SetChannel(channelA).
-				SetBody(response.Message.Body))
-			if err != nil {
-				log.Fatalf("ERROR-B: QueuesStreamClient.Send failed, err: %v", err)
-			}
-			log.Printf("SEND-B: MessageID: %s, Body: %s", sendResult.MessageID, message)
-		}
-	})
-	if err != nil {
-		log.Fatalf("ERROR-B: TransactionStream failed, err: %v", err)
-	}
-	defer func() { doneB <- struct{}{} }()
 	// Start queuesClientA send routine.
 	var testDoneChan = make(chan struct{})
 	go func() {
@@ -164,7 +117,7 @@ func main() {
 			message := fmt.Sprintf("test_message_%d", i)
 			sendResult, err := queuesClientA.Send(ctx, kubemq.NewQueueMessage().
 				SetId(messageID).
-				SetChannel(channelB).
+				SetChannel(channelA).
 				SetBody([]byte(message)))
 			if err != nil {
 				log.Fatalf("ERROR-A: QueuesStreamClient.Send failed, err: %v", err)
